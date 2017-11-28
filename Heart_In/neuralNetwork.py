54b2ae8cceac22eea7b5cb73f5706e3f942e9183
Oleksii Zaitsev <oleksii.animation@gmail.com>
2017-11-28 23:15:22 +0200

    percentage_of_correct_answers save_weights examine

--- Heart_In/neuralNetwork.py
+++ Heart_In/neuralNetwork.py
@@ -143,3 +143,94 @@ class Training(neuralNetwork):
             #целевое маркерное значение для данной записи
             targets[int(re.findall("(.*)_.*", image)[0])] = 0.99
             self.train(inputs, targets)
+
+    #ПРОВЕРКА НЕЙРОННОЙ СЕТИ И ДООБУЧЕНИЕ
+    def percentage_of_correct_answers(self, path:"...\\examine\\" = "String",use_weights = True) ->"вернет процент правильных ответов":
+        """надо указать путь к папке где картинки 
+       руками, для провеки процента правильных ответов"""
+    
+        wih_file =  open("wih.json", 'r')
+        who_file =  open("who.json", 'r')
+        wih = numpy.array(json.load(wih_file))
+        who = numpy.array(json.load(who_file))
+
+        images = os.listdir(path)
+        number_of_pictures = len(images)
+        guessed = 0
+
+        for image in images:
+            fullpath = path + image 
+            arr = self.query(self.image_ready(fullpath), wih, who)
+            arr_max = numpy.amax(arr)
+            
+            number = int(re.findall("(.*)_.*", image)[0])
+            
+            if numpy.where(arr==arr_max)[0] == number:
+                use_weights = True
+                guessed += 1 
+            else:
+                use_weights = False
+        
+        #считает процент правильных ответов
+        correct_answers = py(guessed *100)/number_of_pictures
+        
+        wih_file.close() 
+        who_file.close()
+        return correct_answers
+
+    def save_weights(self):
+        try:
+            with open("wih.json", 'w') as wih:
+                json.dump(self.wih.tolist(), wih)
+
+        except ValueError:
+            pass
+
+        try:
+            with open("who.json", 'w') as who:
+                json.dump(self.who.tolist(), who)
+        except ValueError:
+            pass
+
+    #проверка исходных весов, если распознала больше чем перед этим, использем веса
+    def examine(self, path:"...\\examine\\" = "String"):  
+        
+        #сохранить базовые случайные веса
+        self.save_weights()
+
+        while True:
+            if int(self.percentage_of_correct_answers(path)) > 70:
+                break
+            else:
+                self.save_weights()
+
+            #первая тренировка на базе случайных весов
+            self.training(r"C:\Users\o.zaitsev\Source\Repos\neuralNetwork\neuralNetwork\numbers\\")
+            print("\tpercentage_of_correct_answers  =", self.percentage_of_correct_answers(path), "\tlr = ", self.lr)
+           
+
+
+        old_correct_answers = self.percentage_of_correct_answers(path)
+        while True:
+            #тренировка на базе обновленных весов(первый раз всегда будет лучше, чем случайные
+            self.training(r"C:\Users\o.zaitsev\Source\Repos\neuralNetwork\neuralNetwork\numbers\\")
+            new_correct_answers = self.percentage_of_correct_answers(path)
+
+
+
+            if int(self.percentage_of_correct_answers(path)) > 80:
+                break
+            #если распознала больше чем перед этим, записываем обновленные веса в файл и уменьшаем шаг
+            if  new_correct_answers >= old_correct_answers:
+                self.save_weights()
+                self.lr -= 0.010
+                old_correct_answers = self.percentage_of_correct_answers(path)
+
+
+            print("old_correct_answers = ", old_correct_answers, "\tpercentage_of_correct_answers  =", new_correct_answers, "\tlr = ", self.lr)
+        print("FINAL RESULT =", new_correct_answers)
+
+
+#examine_images = r"C:\Users\o.zaitsev\Source\Repos\neuralNetwork\neuralNetwork\examine\\"
+run = Training()
+#run.examine(examine_images)
